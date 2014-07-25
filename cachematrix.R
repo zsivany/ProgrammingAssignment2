2a081f281fd5bfba571cfc7aff3161291e4dc1b8
zsivany <tamaspapp84@gmail.com>
2014-07-25 18:29:19 +0200

    inverse mtx function
    coursera - ASS 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 ##This Programming assignment two for time consuming computation task
-## Here is my solution
-##@PT
 
-makeCacheMatrix <- function(x = matrix()) {
+##PT
 
+## makeCacheMatrix input variable a matrix object then the inside of the function calculate the inverse of the mtx
+
+makeCacheMatrix <- function(c(data.matrix, m){
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+	##list for the 
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
+##cacheSolve calculate the inverse of the matrix
+##First check, the inverse of the matrix is calculated already. If it true, then the computation of the
+## inverse matrix will skipped
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        m <- x$getinverse()
+        if (!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setsolve(m)
 }
